fd0b3ed59e9f071d46ae01e60aa340ea237ab425
Ashish Sahani <ashish.sahani.7777@gmail.com>
2018-03-30 21:22:52 +0530

    function component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
     render() {
-        // return ( <div className = "App" >
-        //     <h1> Hello in react </h1> </div >
-        // );
-        return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Hello from React'))
+        return (
+          // we don't have to add a class attribute
+          // There will be one div for a component to start
+          <div className = "App" >
+            <h1> Hello in react </h1>
+            <Person/>
+          </div >
+        );
+        //return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Hello from React'))
     }
 }
 
