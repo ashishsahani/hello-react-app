5e7d3c05efdad7aacf5ab93edaf735f19574faa6
Ashish Sahani <ashish.sahani.7777@gmail.com>
2018-03-31 00:45:07 +0530

    manipulating state

--- src/App.js
+++ src/App.js
@@ -9,12 +9,26 @@ class App extends Component {
       {name:'Angel', age : 34}
     ]
   }
+  switchHandlerMethod = ()=> {
+    //console.log('called')
+    // Don't do that
+    // this.state.persons[0].name = 'Kol';
+    // use special property
+    this.setState({
+      persons:[
+        {name:'Jon', age : 29},
+        {name:'Joe', age :13},
+        {name:'Jill', age : 34}
+      ]
+    })
+  }
     render() {
         return (
           // we don't have to add a class attribute
           // There will be one div for a component to start
           <div className = "App" >
             <h1> Hello in react </h1>
+            <button onClick={this.switchHandlerMethod}>Switch</button>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
             <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
